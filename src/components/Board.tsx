d53c5a6ca44bd56641fdfac0a88a618ce5b3e943
grace k <grace.kuperman5472@gmail.com>
2023-02-09 16:41:44 +0000

    flip card function in progress

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,45 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { CardI, makeCards } from "../utils/makeCards";
 import "./Board.css";
 
 export function Board(): JSX.Element {
-  const allCards: CardI[] = makeCards();
-  const [cardsInPlay, setCardsInPlay] = useState<CardI[]>([])
-  
-  
-  //on mount, set the cards in play to my array of emojis  
-  useEffect(()=> {
-    setCardsInPlay(allCards)
-    console.log(setCardsInPlay)
-  }, [])
+  const [cardsInPlay, setCardsInPlay] = useState<CardI[]>(makeCards())
 
 
   const handleCardClick = (cardToFlip: CardI) => {
-    console.log("card has been clicked")
-    // take the id of the card and ... map over the cards in play and return 
-    const cardsPostFlip = cardsInPlay.map((oneCard) => {
-        if (cardToFlip.id === oneCard.id){
-            cardToFlip.life === "faceUp"
+    console.log(cardToFlip.id, "card has been clicked")
+    // take the id of the card and ... map over the cards in play and return :  return {...oneCard, life:"faceUp"}
+    const cardsPostFlip: CardI[] = getCardswOneFlipped(cardsInPlay, cardToFlip)
+    setCardsInPlay(cardsPostFlip)
+  }
+// breaking down the flip cards into two different things, fix this return statement
+  const getCardswOneFlipped = (cards: CardI[], targetCard: CardI): CardI[] => {
+    const copyOfCards = [...cards]
+    copyOfCards.map((oneCard) => {
+        if (targetCard.id === oneCard.id){
+            console.log(targetCard.emoji)
+            return oneCard.life === "faceUp"
+           
         }
     })
-    //switch the life property to faceUp
-    // update boardState to ... 
-        // if gameState is non-clicked => update game state as one-click
-        //if gameState is one-clicked => update game state to two-clicked
-        //if game state is two-clicked => " you cannot do that"
-    return cardsPostFlip
+    console.log("i am copy of cards", copyOfCards)
+    return copyOfCards
   }
-
 
   return (
     <>
     <h1 className= "title"> MATCHING GAME </h1>
 
       <div className="ctn-game-board">
-        {allCards.map((card, i) => {
+        {cardsInPlay.map((card, i) => {
           return (
-            <div key={card.id} className={`card${card.life}`} >
-                {card.life === "faceUp" && <p onClick={() => handleCardClick(card)}> {card.emoji} </p>} 
+            <div key={card.id} className={`card${card.life}`} onClick={() => handleCardClick(card)}>
+                {card.life === "faceUp" && <p > {card.emoji} </p>} 
             </div>
           );
         })}
@@ -47,3 +42,9 @@ export function Board(): JSX.Element {
     </>
   );
 }
+
+   //switch the life property to faceUp
+    // update boardState to ... 
+        // if gameState is non-clicked => update game state as one-click
+        //if gameState is one-clicked => update game state to two-clicked
+        //if game state is two-clicked => " you cannot do that"
